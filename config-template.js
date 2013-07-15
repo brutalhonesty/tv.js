6e598033ec056c98c674532ac42389bb5446c73d
Schodde <sparky1010@gmail.com>
2013-07-15 12:40:32 -0700

    added picking for branch

---
+++ config-template.js
@@ -0,0 +1,18 @@
+module.exports =  {
+	/* Port */
+	port: process.env.PORT || 8888,
+
+	/* Debug mode */
+	debug: true,
+	/**/
+	ptp: {
+		username: '',
+		password: '',
+		passkey: ''
+	},
+	bb: {
+		username: '',
+		password: '',
+		passkey: ''
+	}
+};
